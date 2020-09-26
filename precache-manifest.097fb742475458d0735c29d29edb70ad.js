d7eba7361a37a0ee07068e76b60682ffd85a71b7
Danish Sharma <danishsharma1999@gmail.com>
2020-09-26 02:29:59 -0400

    Updates

---
+++ precache-manifest.097fb742475458d0735c29d29edb70ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7be92bc8e93d2a1c9ca06628100a149",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1661d697b6cf7300dbed",
+    "url": "/robofriends/static/css/main.7c074063.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1661d697b6cf7300dbed",
+    "url": "/robofriends/static/js/main.d6beef4a.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
